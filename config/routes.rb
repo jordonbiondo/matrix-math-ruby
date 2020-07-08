3a0a8185732775a9c6594838da9fc7477e6c779e
jordonbiondo <jordonbiondo@gmail.com>
2020-07-08 17:04:17 -0400

    some db config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: redirect('/api/matrix/compute')
+
+  namespace :api do
+      resources :matrix do
+        get :compute, on: :collection
+      end
+    end
+
 end
