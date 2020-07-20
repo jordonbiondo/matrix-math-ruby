12380cc6fc64f09a8e9a4e52bf1924dec8cc2a17
jordonbiondo <jordonbiondo@gmail.com>
2020-07-20 14:55:42 -0400

    fix indent

--- server/app/controllers/api/matrix_controller.rb
+++ server/app/controllers/api/matrix_controller.rb
@@ -10,19 +10,19 @@ class Api::MatrixController < ApplicationController
     solutions = rref.number_of_solutions
     (is_lin_ind, reason) = matrix.linearly_independent?
     render json: {
-             matrix: matrix.data,
-             size: matrix.size,
-             rref: {data: rref.data},
-             det: matrix.determinant,
-             inverse: matrix.invertible? ? {data: matrix.inverse.data} : nil,
-             lin_ind: {
-               is_linearly_independent: is_lin_ind,
-               reason: reason,
-             },
-             solutions: {
-               value: (solutions == Float::INFINITY ? -1 : solutions)
-             }
-           }
+      matrix: matrix.data,
+      size: matrix.size,
+      rref: {data: rref.data},
+      det: matrix.determinant,
+      inverse: matrix.invertible? ? {data: matrix.inverse.data} : nil,
+      lin_ind: {
+        is_linearly_independent: is_lin_ind,
+        reason: reason,
+      },
+      solutions: {
+        value: (solutions == Float::INFINITY ? -1 : solutions)
+      }
+    }
   end
 
 end
